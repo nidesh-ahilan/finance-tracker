f0634defe124a9e9249c1edd3c308910ab4d465a
nidesh-ahilan <nideshahilan27@gmail.com>
2025-02-22 14:18:36 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { HashRouter as Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Login from './Components/Login/Login';
 
